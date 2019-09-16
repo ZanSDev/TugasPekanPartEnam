e294c2bff695e46946167678c291337d326d5694
ZanSDev <zan.mobileprojek@gmail.com>
2019-09-16 10:27:54 +0700

    Crud Retrofit2

---
+++ app/src/main/java/com/example/mynotes/activity/editor/EditorPresenter.java
@@ -0,0 +1,118 @@
+package com.example.mynotes.activity.editor;
+
+import androidx.annotation.NonNull;
+
+import com.example.mynotes.api.ApiClient;
+import com.example.mynotes.api.ApiInterface;
+import com.example.mynotes.model.Note;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EditorPresenter {
+
+    private EditorView view;
+
+    public EditorPresenter(EditorView view) {
+        this.view = view;
+    }
+
+    void saveNote(final String title, final String note, final int color) {
+
+        view.showProgress();
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<Note> call = apiInterface.saveNote(title, note, color);
+
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+                view.hideProgress();
+
+                if (response.isSuccessful() && response.body() != null) {
+
+                    Boolean success = response.body().getSuccess();
+                    if (success) {
+                        view.onRequestSuccess(response.body().getMessage());
+
+                    } else {
+                        view.onRequestError(response.body().getMessage());
+
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+                view.hideProgress();
+                view.onRequestSuccess(t.getLocalizedMessage());
+
+            }
+        });
+
+    }
+
+    void updateNote(int id, String title, String note, int color) {
+
+        view.showProgress();
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        Call<Note> call = apiInterface.updateNote(id, title, note, color);
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+                view.hideProgress();
+                if (response.isSuccessful() && response.body() != null) {
+
+                    Boolean success = response.body().getSuccess();
+                    if (success) {
+                        view.onRequestSuccess(response.body().getMessage());
+                    } else {
+                        view.onRequestError(response.body().getMessage());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+                view.hideProgress();
+                view.onRequestError(t.getLocalizedMessage());
+            }
+        });
+
+    }
+
+    void deleteNote(int id){
+        view.showProgress();
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<Note> call = apiInterface.deleteNote(id);
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse( @NonNull Call<Note> call, @NonNull Response<Note> response) {
+                view.hideProgress();
+                if (response.isSuccessful() && response.body() != null){
+
+                    Boolean success = response.body().getSuccess();
+                    if (success) {
+                        view.onRequestSuccess(response.body().getMessage());
+                    } else {
+                        view.onRequestError(response.body().getMessage());
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure( @NonNull Call<Note> call, @NonNull Throwable t) {
+                view.hideProgress();
+                view.onRequestError(t.getLocalizedMessage());
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/mynotes/activity/editor/EditorView.java
@@ -0,0 +1,10 @@
+package com.example.mynotes.activity.editor;
+
+public interface EditorView {
+
+    void showProgress();
+    void hideProgress();
+    void onRequestSuccess(String message);
+    void onRequestError(String message);
+
+}
